a13626a9c5c9759f1bcb0dcbabd7f9e651a1af78
nikhiljose1993 <nikhiljose18@gmail.com>
2022-12-22 02:46:22 +0530

    Added endpoint for saving registration data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2/promise");
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
 
 const app = express();
 const port = 3000;
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+const phoneRegex = /^\+?[0-9]{1,2}[0-9]{10}/;
+const salt = "dhedr2tr43@5";
 
 const setConnection = async () => {
   return await mysql.createConnection({
@@ -16,7 +21,100 @@ const setConnection = async () => {
   });
 };
 
-app.get("/getdetails/:hospitalId", async (req, res) => {
+const validatePassword = (password, validationErrors) => {
+  if (password.length < 8) {
+    validationErrors.push({
+      passwordMinLengthError: "Password needs a minimum of 8 characters",
+    });
+  }
+  if (password.length > 15) {
+    validationErrors.push({
+      passwordMaxLengthError: "Password maximum length should be 15 characters",
+    });
+  }
+  if (!/[0-9]+/.test(password)) {
+    validationErrors.push({
+      passwordNumberError: "Password should contain atleast one number",
+    });
+  }
+  if (!/[A-Z]+/.test(password)) {
+    validationErrors.push({
+      passwordUpperCaseError:
+        "Password should contain atleast one uppercase character",
+    });
+  }
+  if (!/[a-z]+/.test(password)) {
+    validationErrors.push({
+      passwordLowerCaseError:
+        "Password should contain atleast one lowercase character",
+    });
+  }
+};
+
+app.post("/register-patients", async (req, res) => {
+  const psychiatristId = req.query.id;
+  const validationErrors = [];
+  const { name, address, email, phoneNumber, password } = req.body;
+  if (!name) {
+    validationErrors.push({ nameError: "Need a name" });
+  }
+  if (address.length < 10) {
+    validationErrors.push({
+      addressError: "Address needs a minimum of 10 characters",
+    });
+  }
+  if (!emailRegex.test(email)) {
+    validationErrors.push({ emailError: "Email address is not valid" });
+  }
+  if (phoneNumber && !phoneRegex.test(phoneNumber)) {
+    validationErrors.push({
+      phoneNumberError: "Phonenumber is not valid",
+    });
+  }
+  validatePassword(password, validationErrors);
+
+  if (validationErrors.length) {
+    console.log(validationErrors);
+    res.status(422);
+    res.send(validationErrors);
+    return;
+  }
+
+  const hash = crypto
+    .pbkdf2Sync(password, salt, 1000, 64, `sha512`)
+    .toString(`hex`);
+
+  try {
+    const connection = await setConnection();
+    await connection.query(
+      `INSERT INTO patients
+      (
+        name,
+        address,
+        email,
+        phone,
+        password,
+        psychiatrists_id
+      )
+      VALUES
+      (
+        '${name}',
+        '${address}',
+        '${email}',
+        '${phoneNumber || null}',
+        '${hash}',
+        ${psychiatristId}
+      );`
+    );
+    res.send("Successfully registered patient");
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    res.send(error);
+  }
+});
+
+app.get("/get-details/:hospitalId", async (req, res) => {
   const hospitalId = req.params.hospitalId;
   try {
     const connection = await setConnection();
@@ -48,9 +146,10 @@ app.get("/getdetails/:hospitalId", async (req, res) => {
       totalPatientsCount,
       psychiatristsDetails,
     });
-  } catch (err) {
-    console.log(err);
-    res.send(err);
+  } catch (error) {
+    console.log(error);
+    res.status(400);
+    res.send(error);
   }
 });
 
@@ -59,9 +158,9 @@ app.get("/get", async (req, res) => {
     const connection = await setConnection();
     const [result] = await connection.query("select * from patients");
     res.send(result);
-  } catch (err) {
-    console.log(err);
-    res.send(err);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
   }
 });
 
